9d2db44bcfee5ea8ee6747686714b64548e977fb
Admeya <lugovova@live.ru>
2016-04-18 23:25:12 +0300

    Задание 5 - Рефакторинг кода, добавление click(), type(), обработка alert всплывающего окна при удалении контанта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -2,27 +2,25 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
 
 /**
  * Created by Home on 18.04.2016.
  */
-public class NavigationHelper {
-  private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void goToGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 
   public void goToAddNewContact() {
-    new Actions(wd).doubleClick(wd.findElement(By.linkText("add new"))).build().perform();
+    click(By.linkText("add new"));
   }
 
   public void goToHomePage() {
-      wd.findElement(By.linkText("home")).click();
+    click(By.linkText("home"));
   }
 }

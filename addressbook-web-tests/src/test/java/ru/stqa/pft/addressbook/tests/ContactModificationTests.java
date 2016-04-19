be0e8ea81da58cf72745e1d2952e71ef3ec1fa77
Admeya <lugovova@live.ru>
2016-04-19 09:09:40 +0300

    Задание 6 - созданы тесты для модификации групп и контактов.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+/**
+ * Created by Home on 19.04.2016.
+ */
+public class ContactModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().goToHomePage();
+    app.getContactHelper().selectContact();
+
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactInfo(new ContactData("Антуан", "Сергеевич", "Заляутдинов", "Zollas", "специалист", "Dosker", "Москва, 5 Пролетарский проезд, д. 17, кв. 1", "523-43-65", "89908761223"));
+    app.getContactHelper().submitContactModification();
+  }
+}

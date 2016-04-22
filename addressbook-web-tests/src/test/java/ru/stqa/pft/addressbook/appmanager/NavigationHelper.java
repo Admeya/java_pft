02f352decea5043743a5d386a01c93a22b5a06ae
Admeya <lugovova@live.ru>
2016-04-22 13:54:18 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -13,7 +13,12 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void goToGroupPage() {
-    click(By.linkText("groups"));
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))){
+      return;
+    }
+      click(By.linkText("groups"));
   }
 
   public void goToAddNewContact() {
@@ -21,6 +26,9 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void goToHomePage() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 }

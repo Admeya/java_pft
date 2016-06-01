ae3542bf47920ae3137436309c1dcf11f9c06c09
Admeya <lugovova@live.ru>
2016-06-01 17:25:13 +0300

    Протокол ftp, отключение капчи

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 import static ru.stqa.pft.mantis.tests.TestBase.app;
 
-public class RegistrationTests {
+public class RegistrationTests extends TestBase {
 
     @Test
     public void testRegistration(){

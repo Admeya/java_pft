8ab1a87143baf36c97afc63edbca086f3cca819e
Admeya <lugovova@live.ru>
2016-06-01 16:34:06 +0300

    Добавили ленивую инициализацию

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.HttpSessionId;
 
 import java.io.File;
 import java.io.FileReader;
@@ -17,35 +18,56 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplicationManager {
   private Properties properties;
-  WebDriver wd;
+  private WebDriver wd;
 
-  private String browser;
+    private String browser;
+    private RegistrationHelper registrationHelper;
 
-
-  public ApplicationManager(String browser){
-    this.browser = browser;
-    properties = new Properties();
-  }
-
-   public void init() throws IOException {
-    String target = System.getProperty("target", "local");
-    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
-
-    if (browser.equals(BrowserType.FIREFOX)){
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)){
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)){
-      wd = new InternetExplorerDriver();
+    public ApplicationManager(String browser){
+        this.browser = browser;
+        this.properties = new Properties();
     }
 
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
     }
 
   public void stop() {
-    wd.quit();
+    if (wd != null) {
+        wd.quit();
+    }
   }
 
+    public HttpSession newSession(){
+        return new HttpSession(this);
+    }
 
+    public String getProperty(String key){
+        return properties.getProperty(key);
+    }
+
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null ) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null){
+            if (browser.equals(BrowserType.FIREFOX)){
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)){
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)){
+                wd = new InternetExplorerDriver();
+            }
+
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
+    }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -2,30 +2,23 @@ package ru.stqa.pft.mantis.tests;
 
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
-
-
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 
 public class TestBase {
 
   protected static final ApplicationManager app
-          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
-  @AfterSuite
+  @AfterTest
   public void tearDown() {
     app.stop();
   }
 
 
-  @BeforeSuite
+  @BeforeTest
   public void setUp() throws Exception {
     app.init();
   }
